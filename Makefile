7eb4b4afb908c959424c9a7555c8734b7a9a9ae9
Alexander Dmitriev <alexander.dmitriev@openinnovation.ai>
2026-02-14 13:28:43 +0400

    lab2

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 mypy:
 	mypy lab1
 	cd lab1/admin_learning && mypy admin_learning
+	cd lab2/helloworld && mypy helloworld
---
+++ lab2/helloworld/flatpages/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, "index.html", {})
